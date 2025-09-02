8456653542d4827eee434ebc97ade6d03dec43c2
conrado <cblanco@alumnos.exa.unicen.edu.ar>
2025-09-02 03:08:52 -0300

    fix service

--- src/main/java/com/conrado/couponapi/service/CouponService.java
+++ src/main/java/com/conrado/couponapi/service/CouponService.java
@@ -1,12 +1,10 @@
 package com.conrado.couponapi.service;
 
 import com.conrado.couponapi.model.dto.CouponResponse;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@Service
 public interface CouponService {
     CouponResponse calculateCoupon (List<String> itemIds, BigDecimal amount);
 }
